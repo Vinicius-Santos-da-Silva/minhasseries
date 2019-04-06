5b22e88d3d9d2c65a1c5cd9d1c3420867251f398
Vinicius <vinicius.unisinos@hotmail.com>
2019-04-06 17:24:55 -0300

    Login

--- src/APIs.js
+++ src/APIs.js
@@ -1,13 +1,16 @@
 import axios from 'axios'
 
 const api = axios.create({
-	baseURL:'http://ec2-3-16-89-162.us-east-2.compute.amazonaws.com/api-twitter-codeigniter/index.php/'
+	baseURL:'http://ec2-3-16-89-162.us-east-2.compute.amazonaws.com/api-twitter-codeigniter/index.php/',
+	headers: {
+		'Content-Type': 'application/json',
+	}
 })
 
 export const loadGenres 		= () 		 => api.get('genres')
 export const saveSerie 			= (newSerie) => api.post('series',newSerie);
 export const loadGenresByGenre 	= (genre) 	 => api.get('series?genre='+genre);
-export const login 				= (login) 	=> api.post('login', login);
+export const login 				= (login) 	 => api.post('login', login);
 
 const apis = {
 	loadGenres : loadGenres,
